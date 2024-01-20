058e51b639c14a0481945ff647ef470cb2822be2
imocsobota <csobota@imohealth.com>
2024-01-20 09:37:48 -0600

    Assignment1

---
+++ CSC500/Assignment1/part_2.py
@@ -0,0 +1,19 @@
+'''
+    Multiply two numbers together and print result
+        Input:
+            first: First number
+            second: Second number
+
+'''
+def multiply_numbers(first,second):
+    print('\n\n================================================')
+    print('\nThe product of the two numbers is: ' + str(first * second))
+
+
+'''
+    Divid the two numbers and print results
+'''
+def divide_numbers(first,second):
+    print('The first number divided by the second number is: ' + str(first / second))
+    print('The second number divided by the first number is: ' + str(second / first))
+    print('\n================================================')
